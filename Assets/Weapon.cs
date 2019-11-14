6fae69f934e5384462462666d09a827a72a0a88a
jonathan <jonathan.fullola>
2019-11-14 02:30:26 +0100

    force + muzzel flash

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -7,63 +7,56 @@ public class Weapon : MonoBehaviour
     public float fireRate = 0;
     public float Damage = 10;
     public LayerMask whatToHit;
+    GameObject player;
 
     float timeToFire = 1.5f;
 
     Transform firePoint;
     GameObject blast;
 
+    public Transform BulletTrailMuzzel;
+    float timeToSpawnEffect = 0;
+    public float effectSpawnRate = 10;
+
     void Awake()
     {
         firePoint = transform.FindChild("FirePoint");
         blast = GameObject.Find("blast");
-    }
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (fireRate == 0)
-        {
-            if (Input.GetButtonUp("Fire1"))
-            {
-                Debug.Log("button up");
-                blast.GetComponent<Animator>().SetInteger("blastInt",1);
-                Shoot();
-                if (blast.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("blast"))
-                {
-                    blast.GetComponent<Animator>().SetInteger("blastInt", 0);
-                }
-            }
-        }else
-        {
-            if(Input.GetButtonDown("Fire1") && Time.time > timeToFire)
+            if(Input.GetButtonUp("Fire1") && Time.time > timeToFire && Time.time >= timeToSpawnEffect)
             {
                 timeToFire = Time.time + 1 / fireRate;
-                Shoot();
+                ShootMuzzel();
+                timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
             }
-        }
-
     }
 
  
 
-    void Shoot()
+    void ShootMuzzel()
     {
         Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
         Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
 
+        Effect();
+           
         RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
         Debug.DrawLine(firePointPosition, mousePosition,Color.cyan);
         if (hit.collider != null)
         {
             Debug.DrawLine(firePointPosition, hit.point, Color.red);
         }
+        player.GetComponent<Rigidbody2D>().AddForce(new Vector2(-(mousePosition - firePointPosition).normalized.x * 1000, -(mousePosition - firePointPosition).normalized.y * 250) );
     }
+
+    void Effect()
+    {
+        Object.Instantiate(BulletTrailMuzzel, firePoint.position, firePoint.rotation,this.transform);
+    }
+
 }
